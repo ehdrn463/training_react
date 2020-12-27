dce92fc050ee99d10bb5adbbda3f42338f99e216
DongGu <god.donggu@gmail.com>
2020-12-27 23:14:22 +0900

    #8.2 useEffect v2

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickName] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);

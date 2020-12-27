98ece7b6544538820c821d028973566bfafa70f2
DongGu <god.donggu@gmail.com>
2020-12-28 08:15:43 +0900

    #8.4 useMemo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React, { useState } from 'react';
-import Info from './Info';
+import React from 'react';
+import Average from './Average';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  console.log(visible);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Average />;
 };
 
 export default App;

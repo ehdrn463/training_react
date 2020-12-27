cfec39a87d7fa0698b22387e5154c20170387bed
DongGu <god.donggu@gmail.com>
2020-12-27 22:49:44 +0900

    #8.2 useEffect

---
+++ src/Info.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Info = () => {
+  const [name, setName] = useState('');
+  const [nickname, setNickName] = useState('');
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickName(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <input value={name} onChange={onChangeName} />
+          <input value={nickname} onChange={onChangeNickname} />
+        </div>
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+      </div>
+      <div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
